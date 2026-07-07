881bc8e0f4bc055d1ddb7d14181fcd9556f0987a
Keeseok Jeong <kjeong.app@gmail.com>
2026-07-06 17:25:38 -0700

    Deploy timer skin options

--- cdx_timer/flutter_bootstrap.js
+++ cdx_timer/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4029772260" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1615756226" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
