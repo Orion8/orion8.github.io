4ca04f63a79f7c8e01fc857c0d1eb9d4e1ed9088
Keeseok Jeong <kjeong.app@gmail.com>
2026-07-09 20:19:22 -0700

    Deploy Time Map v0.4.0 spatial canvas

--- cdx_time_map/flutter_bootstrap.js
+++ cdx_time_map/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "126599053" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3672779922" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
