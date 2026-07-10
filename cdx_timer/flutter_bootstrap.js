d6196d35fecbe8c304794373c0e8db9183c1cdc8
Keeseok Jeong <kjeong.app@gmail.com>
2026-07-09 21:54:24 -0700

    Deploy visible swimmer splashes

--- cdx_timer/flutter_bootstrap.js
+++ cdx_timer/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1868031062" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2010057524" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
