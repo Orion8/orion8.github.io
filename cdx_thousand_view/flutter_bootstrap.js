d6ac12332b40c0b7c2d9983f4cfd741c492bded7
Keeseok Jeong <kjeong.app@gmail.com>
2026-07-16 18:12:57 -0700

    Deploy Thousand View v5.3

--- cdx_thousand_view/flutter_bootstrap.js
+++ cdx_thousand_view/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1200125651" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2913853718" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
