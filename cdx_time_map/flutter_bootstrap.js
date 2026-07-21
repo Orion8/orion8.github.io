42a1bfbfba54bd4f40dd14966ba4091b93f04f92
Keeseok Jeong <kjeong.app@gmail.com>
2026-07-20 18:38:58 -0700

    Publish Sumerian civilization catalog

--- cdx_time_map/flutter_bootstrap.js
+++ cdx_time_map/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4123890" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "833730049" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
