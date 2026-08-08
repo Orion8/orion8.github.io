302226dd8cdfad581cbafeb228564f66597fbf45
Keeseok Jeong <kjeong.app@gmail.com>
2026-08-08 09:43:10 -0700

    Deploy Time Map v0.35.2

--- cdx_time_map/flutter_bootstrap.js
+++ cdx_time_map/flutter_bootstrap.js
@@ -48,6 +48,6 @@ for (const build of _flutter.buildConfig.builds) {
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1110284603" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "1709386042" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
